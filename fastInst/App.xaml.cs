8e4d5059186ea336a165851760badb5709777417
GOTOUMA-KAKERU <43anillo@gmail.com>
2025-06-08 05:25:27 +0900

    プロジェクト ファイルを追加します。

---
+++ fastInst/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace fastInst
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
